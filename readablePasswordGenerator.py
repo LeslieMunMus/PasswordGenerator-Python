dc87c7851055110a6664ab53f191077f11200ae3
LeslieMunMus <leslie.musengi@icloud.com>
2021-03-30 23:45:18 +0200

    Updated the len(word) to > 8 chars

--- readablePasswordGenerator.py
+++ readablePasswordGenerator.py
@@ -21,7 +21,7 @@ with open("Wikitext.txt", "r") as file:
         words = line.split()  # Splitting the sentences into words
 
         for word in words:
-            if len(word) > 5:
+            if len(word) > 8:
                 wordlist.append(word.capitalize())  # Capitalizing each word in the list and appending to wordlist
 
 word_char = random.choice(wordlist)
